6dba97cd73eb055eae5ecf2d1ff7db835ebdfb54
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-09-06 13:06:50 -0600

    Up to video 25
    -Connected search API to VideoList and VideoListItem
    -Added markup to properly display the items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyAegbYh8SCCIaspv9ltN_q6nBb-o1Omzhc';
 
 // Create a new component.
 // This component should produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'boosted board'}, (videos) => {
+      this.setState({ videos }); // this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
